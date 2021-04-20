3ba2f9a7832e55b67157368612e20681b547166e
Adrian <adriansiv97@hotmail.com>
2021-04-20 16:42:29 +0200

    Done

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const URL =
   "https://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api/photos.json";
 
+  const numberOfColumns = 4;
 /*
+!!! HINT: REMEMBER THE STRUCTURE !!!
 fetch('url')
 .then(Resolve)
 .then(Reject) OR .catch(Reject)
@@ -12,4 +14,46 @@ fetch('url').then(ResolveFuntion, RejectFuntion)
 
 */
 
-const app = document.querySelector("#app");
+
+
+const app = document.getElementById('app');
+
+const divRow = document.createElement("div");
+divRow.className = 'row';
+
+const columns = [];
+
+const createColumns = () => {
+  for (let i = 0; i <= numberOfColumns -1; i++) {
+    const divColumn = document.createElement("div");
+    divColumn.className = "column";
+    divRow.appendChild(divColumn);
+    columns.push(divColumn);
+  }
+}
+createColumns();
+
+fetch(URL)
+.then(response => onSuccessFetch(response))
+.then(error => console.log('this is an error: ', error));
+
+const onSuccessFetch = (resolve) => {
+  resolve.json()
+  .then((photos) => {
+    for (indexOfThePhoto in photos) {
+
+      const { author, url, pubdate } = photos[indexOfThePhoto]
+      
+      const img = document.createElement("img");
+      img.src = url;
+
+      const p = document.createElement("p");
+      p.innerText = `Author: ${author}`;
+
+      columns[indexOfThePhoto % numberOfColumns].appendChild(img);
+      columns[indexOfThePhoto % numberOfColumns].appendChild(p);
+
+      app.appendChild(divRow);
+    }
+  })
+};
